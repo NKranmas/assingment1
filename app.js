1d933d4527734bcb0968afa57f56054883bffe26
Kranmas <njkranmas@labs.neit.edu>
2019-03-14 12:38:37 -0400

    more dependencies

--- app.js
+++ app.js
@@ -2,29 +2,34 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var port = 3000;
+var mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var passport = require('passport');
 
-//app.set('view engine', 'html');
+app.set('view engine', 'handlebars');
 
 router.get('/',function(req,res){
-    res.sendFile((__dirname+'/index.html'));
+    res.sendFile((__dirname+'/index'));
   });
 
 router.get('/addgame',function(req,res){
-    res.sendFile((__dirname+'/addgame.html'));
+    res.sendFile((__dirname+'/addgame'));
   });
 
 router.get('/login',function(req,res){
-    res.sendFile((__dirname+'/login.html'));
+    res.sendFile((__dirname+'/login'));
   });
 
 app.post('/addgame', function(req,res){
-    res.sendFile((__dirname+'/index.html'));
+    res.sendFile((__dirname+'/index'));
 });
 app.post('/login', function(req,res){
-    res.sendFile((__dirname+'/index.html'));
+    res.sendFile((__dirname+'/index'));
 });
 
-router.get(express.static(__dirname + '/pages'));
+app.use(express.static(__dirname + '/views'));
+//router.get(express.static(__dirname + '/pages'));
 app.use('/', router);
 
 app.listen(port, function(){
